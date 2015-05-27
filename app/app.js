befcfe9d5e80a5272396d32ee20920cb70424a75
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-05-27 11:38:40 -0400

    wizard now working with animation

--- app/app.js
+++ app/app.js
@@ -6,81 +6,8 @@ angular.module('myApp', [
   'myApp.home',
   'myApp.register',
   'myApp.welcome',
-  'myApp.addPost',
-  'ngAnimate',
-  'ui.bootstrap'
-])
-	.controller('AppCtrl', function ($modal) {
-        var app = this;
-
-        app.closeAlert = function () {
-            app.reason = null;
-        };
-
-        app.open = function () {
-            var modalInstance = $modal.open({
-                templateUrl: 'partials/wizard.html',
-                controller: 'ModalCtrl',
-                controllerAs: 'modal'
-            });
-
-            modalInstance.result
-                .then(function (data) {
-                    app.closeAlert();
-                    app.summary = data;
-                }, function (reason) {
-                    app.reason = reason;
-                });
-        };
-    })
-    .controller('ModalCtrl', function ($modalInstance) {
-        var modal = this;
-
-        modal.steps = ['one', 'two', 'three'];
-        modal.step = 0;
-        modal.wizard = {tacos: 2};
-
-        modal.isFirstStep = function () {
-            return modal.step === 0;
-        };
-
-        modal.isLastStep = function () {
-            return modal.step === (modal.steps.length - 1);
-        };
-
-        modal.isCurrentStep = function (step) {
-            return modal.step === step;
-        };
-
-        modal.setCurrentStep = function (step) {
-            modal.step = step;
-        };
-
-        modal.getCurrentStep = function () {
-            return modal.steps[modal.step];
-        };
-
-        modal.getNextLabel = function () {
-            return (modal.isLastStep()) ? 'Submit' : 'Next';
-        };
-
-        modal.handlePrevious = function () {
-            modal.step -= (modal.isFirstStep()) ? 0 : 1;
-        };
-
-        modal.handleNext = function () {
-            if (modal.isLastStep()) {
-                $modalInstance.close(modal.wizard);
-            } else {
-                modal.step += 1;
-            }
-        };
-
-        modal.dismiss = function(reason) {
-            $modalInstance.dismiss(reason);
-        };
-    })
-
-.config(['$routeProvider', function($routeProvider) {
+  'myApp.addPost'
+]).
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
